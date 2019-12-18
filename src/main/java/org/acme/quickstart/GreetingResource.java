653fb473614cbf267421b2a2bd438d29ddba0d0d
Thomas Darimont <thomas.darimont@codecentric.de>
2019-12-18 21:06:09 +0100

    Revise example GreetingResource and Tests
    - Show current time and container hostname
    - Remove deprecations

--- src/main/java/org/acme/quickstart/GreetingResource.java
+++ src/main/java/org/acme/quickstart/GreetingResource.java
@@ -4,13 +4,28 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.time.Instant;
 
 @Path("/hello")
 public class GreetingResource {
 
-    @GET
-    @Produces(MediaType.TEXT_PLAIN)
-    public String hello() {
-        return "hello";
+  private static final String HOSTNAME;
+
+  static {
+    String hostname;
+    try {
+      hostname = InetAddress.getLocalHost().getHostName();
+    } catch (UnknownHostException e) {
+      hostname = "UNKNOWN";
     }
+    HOSTNAME = hostname;
+  }
+
+  @GET
+  @Produces(MediaType.TEXT_PLAIN)
+  public String hello() {
+    return "hello " + Instant.now() + " Instance=" + HOSTNAME;
+  }
 }
